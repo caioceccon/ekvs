2736a87b52fed7c30129da541905470f32975ddd
Caio Ceccon <contact@caioceccon.com>
2015-05-17 23:03:49 +0200

    Include Cowboy skeleton

---
+++ src/ekvs.erl
@@ -0,0 +1,10 @@
+-module(ekvs).
+
+-export([start/0]).
+
+start() ->
+    ok = application:start(crypto),
+    ok = application:start(ranch),
+    ok = application:start(cowboy),
+    ok = application:start(ekvs).
+
