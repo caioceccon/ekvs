5744ccb2e9a97c53299e51e201932055b66794e5
Caio Ceccon <contact@caioceccon.com>
2015-05-18 18:49:17 +0200

    Add telnetserv supervisor

--- src/ekvs_sup.erl
+++ src/ekvs_sup.erl
@@ -23,4 +23,8 @@ start_link() ->
 %% ===================================================================
 
 init([]) ->
-    {ok, { {one_for_one, 5, 10}, [?CHILD(ekvs_db, worker)]} }.
+    {ok, {{one_for_one, 5, 10},
+          [?CHILD(ekvs_db, worker),
+           ?CHILD(ekvs_telnetserv_sup, supervisor)]
+         }
+    }.
